ebd1ede6fc4e25bf8242f21670d604dd0490c3d1
rafaelurben <github@rafaelurben.ch>
2021-02-04 12:57:54 +0100

    Updated command line utlity

--- pystrom/console.py
+++ pystrom/console.py
@@ -9,14 +9,20 @@ from pystrom.search import MyStromSearch
 from pystrom.utils import log
 
 class MyStromCommandParser(object):
+    """MyStrom command line utility"""
+
+    COMMANDS = ["search"]
+
+    def __get_usage__(self):
+        doc = "pystrom <command> [args]\n\nPossible commands:\n"
+        for cmd in self.COMMANDS:
+            doc += cmd.ljust(12)+getattr(self, cmd).__doc__+"\n"
+        return doc+"\n"
+
     def __init__(self):
         parser = argparse.ArgumentParser(
-            description='[PyStrom] Control MyStrom devices',
-            usage="""pystrom <command> [args]
-            
-Possible commands:
-  search      Search MyStrom devices in your local network
-            """)
+            description='Control MyStrom devices',
+            usage=self.__get_usage__())
         parser.add_argument('command', help='Subcommand to run')
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
@@ -25,10 +31,13 @@ Possible commands:
             exit(1)
         getattr(self, args.command)()
 
+    # Commands
+
     def search(self):
-        parser = argparse.ArgumentParser(
-            description='Record changes to the repository')
-        parser.add_argument('--live', action='store_true', help="Keep connection open until manually stopped.")
+        """Search MyStrom devices in your local network"""
+        parser = argparse.ArgumentParser(description=self.search.__doc__)
+        parser.add_argument('--live', action='store_true', 
+            help="Keep connection open until manually stopped.")
         args = parser.parse_args(sys.argv[2:])
         if args.live:
             MyStromSearch.searchlive()
